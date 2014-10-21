0065b8f194bdd2e3bb9c6b73d6d3813333277059
Andrew Shulman <andy.shulman@hotmail.com>
2014-10-20 23:45:59 -0400

    This will never be serialized

--- src/main/java/com/amshulman/insight/parser/InvalidTokenException.java
+++ src/main/java/com/amshulman/insight/parser/InvalidTokenException.java
@@ -2,9 +2,8 @@ package com.amshulman.insight.parser;
 
 import lombok.Getter;
 
+@SuppressWarnings("serial")
 public class InvalidTokenException extends IllegalArgumentException {
-
-    private static final long serialVersionUID = 2475955056493311236L;
 
     @Getter private final TokenType type;
 
